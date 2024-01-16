a3b1887ae2861d8c6e5dd6f4e42342644a84f63e
saarakhalid <khalidsaara004@gmail.com>
2024-01-16 17:54:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// Declare companyName and set it equal to "Scuber"
+let companyName = 'Scuber';
+
+// Declare companyName again, but as a constant
+const companyNameconst= 'Scuber';
+
+// Declare mostProfitableNeighborhood and set it equal to "Chelsea"
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo and set it equal to "Susan Smith"
+let companyCeo = 'Susan Smith';
