b8c368f74c8c58679ca339063b6d59029265435a
saarakhalid <khalidsaara004@gmail.com>
2024-01-18 11:28:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// Code your solution in this file!
-// Declare companyName and set it equal to "Scuber"
+// Declare a variable companyName and set it equal to 'Scuber'
 let companyName = 'Scuber';
 
-// Declare companyName again, but as a constant
-const companyNameconst= 'Scuber';
+// Declare a variable companyName as a const
+const companyNameconst= 'Scuber'; // This will cause an error since companyName is already declared with let
 
-// Declare mostProfitableNeighborhood and set it equal to "Chelsea"
+// Declare a variable mostProfitableNeighborhood and assign it a value
 let mostProfitableNeighborhood = 'Chelsea';
 
-// Declare companyCeo and set it equal to "Susan Smith"
+// Declare a variable companyCeo and assign it a value
 let companyCeo = 'Susan Smith';
+
+// Output the values to the console for verification
+console.log('companyName:', companyName);
+console.log('mostProfitableNeighborhood:', mostProfitableNeighborhood);
+console.log('companyCeo:', companyCeo);
